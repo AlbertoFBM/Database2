5309d4e20ac72dd1f8b6134fd2727d6c144f7746
DAM <aserrano@cifpfbmoll.eu>
2022-02-23 19:22:28 +0100

    Initial commit

---
+++ app/src/main/java/com/aserrano/database2/WordItem.java
@@ -0,0 +1,27 @@
+package com.aserrano.database2;
+
+public class WordItem {
+
+    private int mId;
+    private String mWord;
+
+    public WordItem() {
+
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getmWord() {
+        return mWord;
+    }
+
+    public void setmWord(String mWord) {
+        this.mWord = mWord;
+    }
+}
---
+++ app/src/main/java/com/aserrano/database2/WordListAdapter.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aserrano.database2;
+
+import android.app.Activity;
+import android.content.Context;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Implements a simple Adapter for a RecyclerView.
+ * Demonstrates how to add a click handler for each item in the ViewHolder.
+ */
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+
+    /**
+     *  Custom view holder with a text view and two buttons.
+     */
+    class WordViewHolder extends RecyclerView.ViewHolder {
+        public final TextView wordItemView;
+        Button delete_button;
+        Button edit_button;
+
+        public WordViewHolder(View itemView) {
+            super(itemView);
+            wordItemView = (TextView) itemView.findViewById(R.id.word);
+            delete_button = (Button)itemView.findViewById(R.id.delete_button);
+            edit_button = (Button)itemView.findViewById(R.id.edit_button);
+        }
+    }
+
+    private static final String TAG = WordListAdapter.class.getSimpleName();
+
+    public static final String EXTRA_ID = "ID";
+    public static final String EXTRA_WORD = "WORD";
+    public static final String EXTRA_POSITION = "POSITION";
+
+    private final LayoutInflater mInflater;
+    Context mContext;
+
+    WordListOpenHelper mDatabase;
+
+    public WordListAdapter(Context context, WordListOpenHelper mDB) {
+        mInflater = LayoutInflater.from(context);
+        mContext = context;
+        mDatabase = mDB;
+    }
+
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+
+        final WordViewHolder h = holder;
+
+        WordItem current = mDatabase.query(position);
+        holder.wordItemView.setText(current.getmWord());
+
+        holder.delete_button.setOnClickListener(new MyButtonOnClickListener(
+                current.getmId(), null){
+            @Override
+            public void onClick(View v) {
+                // Remove from the database
+                int deleted = mDatabase.delete(id);
+                if (deleted >= 0){
+                    notifyItemRemoved(h.getAdapterPosition());
+                }
+            }
+        });
+
+        holder.edit_button.setOnClickListener(new MyButtonOnClickListener(
+                current.getmId(), current.getmWord()){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, EditWordActivity.class);
+
+                intent.putExtra(EXTRA_ID, id);
+                intent.putExtra(EXTRA_POSITION, h.getAdapterPosition());
+                intent.putExtra(EXTRA_WORD, word);
+
+                // Start an empty edit activity
+                ((Activity) mContext).startActivityForResult(intent, MainActivity.WORD_EDIT);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        // Placeholder so we can see some mock data.
+        return (int) mDatabase.count();
+    }
+}
+
+
